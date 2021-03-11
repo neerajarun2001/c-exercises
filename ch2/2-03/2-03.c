e4cc8511b43adaec0205577371dcb4cf664cec61
Neeraj Arunkumar <neerajarun2001@gmail.com>
2021-03-10 23:11:02 -0800

    start hex to int conversion

---
+++ ch2/2-03/2-03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/* 2-03: Write a function htoi(s), which converts a string of hexadecimal digits
+ *       (including an optional 0x or 0X) into its equivalent integer value. The 
+ *       allowable digits are 0 through 9, a through f, and A through F. */
+int htoi(char s[]);
+int main() 
+{
+}
+int htoi(char s[]) 
+{
+	int i, dec;
+
+	dec = 0;
+	for (i=0; s[i] != '\0'; i++) {
+	  	
+	}
+}
