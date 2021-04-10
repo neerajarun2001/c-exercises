0a2243cd1d4c153179ecb0328e0a2438cfa1297c
Neeraj Arunkumar <neerajarun2001@gmail.com>
2021-04-10 11:16:24 -0700

    comments, initialization, input/output stream:

--- learn-cpp/ch1/basics.cpp
+++ learn-cpp/ch1/basics.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+// comments at the program level should explain 'what'
 
-using namespace std;
-
+// at the function level, explain 'how'
 int main()
 {
-	cout << "Hello world!\n";
+	// at the line level, explain 'why'
+	/* 
+	 * can also have multi-line comments
+	 */
+	// before requesting user input, zero initialize x
+	int x{ };
+	// here we use the std::cout variable with the insertion operator
+	std::cout << "Please enter a number" << '\n';
+	// here we use the std::cin variable with the extraction operator
+	std::cin >> x;
+	std::cout << "You entered the number: " << x << '\n';
+	// multi-line definitions are not preferred
+	int a, b;
+	// copy initialization uses '=' at creation. not preferred
+	int c = 5;
+	// direct initialization uses '()' at creation. not preferred. 
+	int d( 5 );
+	// direct list initalization is a better version of direct. PREFERRED.
+	int e{ 5 };
+	// using empty brackets is known as 'zero initialization'
+	int f{ };
 	return 0;
 }
+
