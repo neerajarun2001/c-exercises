58dabdfde2e3fc7b0b5996f77faf53b296706ab8
Neeraj Arunkumar <neerajarun2001@gmail.com>
2021-04-10 12:09:13 -0700

    formatting, operators and literals, identifiers, naming conventions, expressions, statements

--- learn-cpp/ch1/basics.cpp
+++ learn-cpp/ch1/basics.cpp
@@ -25,6 +25,21 @@ int main()
 	int e{ 5 };
 	// using empty brackets is known as 'zero initialization'
 	int f{ };
+	// here's a handy formatting trick to make code more readable
+	// when assigning, tab align things. same idea with comments
+	int variableName 							= 5; 
+	int anotherVariable  					= 5;
+	int moreVariablesToInitialize = 5;
+
+	// string literals on different lines concatenate
+	std::cout << "Check this " 
+		"out" << '\n';
+	// readable comments
+  int i{ 2 };  					// initialize variable x with value 2
+	int j{ 2 + 3}; 				// initialize variable j with value 2 + 3 (5)
+	int k{ (2 * 3) + 4};  // initialize variable k with value 10
+	int h{ i }; 					// initialize variable k with value of i (2)
+	
 	return 0;
 }
 
---
+++ learn-cpp/ch1/build/CMakeFiles/first-program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first-program.dir/first-program.cpp.o"
+  "first-program"
+  "first-program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first-program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
