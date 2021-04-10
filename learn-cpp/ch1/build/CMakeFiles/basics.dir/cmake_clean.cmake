6aeb72debe20ad56262882a9e452e49e1b865588
Neeraj Arunkumar <neerajarun2001@gmail.com>
2021-04-09 20:16:57 -0700

    learncpp.com basics

---
+++ learn-cpp/ch1/build/CMakeFiles/basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basics.dir/basics.cpp.o"
+  "basics"
+  "basics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
