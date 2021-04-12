5dd8c0fae190b9e1af918a4aa21f16ce2ba042f5
Neeraj Arunkumar <neerajarun2001@gmail.com>
2021-04-12 16:23:38 -0700

    enum class and struct

--- learn-cpp/ch8/main.cpp
+++ learn-cpp/ch8/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
+#define TEST
+struct Advertising 
+{
+	int numAds{};
+	double percentAdsClicked{};
+	double earnedPerClick{};
+};
+
+
+void printAdvertisingInfo(Advertising ad) 
+{
+	std::cout << "Ads shown: " << ad.numAds << '\n';
+	std::cout << "Percent clicked: " << ad.percentAdsClicked << '\n';
+	std::cout << "Earned per click: " << ad.earnedPerClick << '\n';
+	
+  double total { ad.numAds * ad.percentAdsClicked * ad.earnedPerClick };
+	std::cout << "Total earned: " << total << '\n';
+
+}
 int main()
 {
+	Advertising advertData{};
+	
+	std::cout << "How many ads have you shown? ";
+	std::cin >> advertData.numAds;
+
+	std::cout << "What percentage of the shown ads have been clicked? ";
+	std::cin >> advertData.percentAdsClicked;
+
+	std::cout << "How much did you make per click? ";
+	std::cin >> advertData.earnedPerClick;
+	
+	printAdvertisingInfo(advertData);
+
+#ifndef TEST
 	// stronger enum
 	enum class Color
 	{
@@ -43,5 +76,7 @@ int main()
 			break;
 			
 	}
+#endif
+	
 	return 0; 
 }
